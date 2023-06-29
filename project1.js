bf88db79e8e2f2fab0c78233d1f0a36df154b717
calllumwhhui <huicallum@gmail.com>
2023-06-29 13:56:12 +0100

    slight refactoring

--- project1.js
+++ project1.js
@@ -28,8 +28,9 @@ function init () {
 
 function flipCard(){
     if (lockBoard) return;
+    // prevents same card being flipped twice. 
     if (this === firstCard) return;
-    // use of the this key word 
+    // use of the this key word, refering to the card object that flipCard is being called on. 
     this.classList.toggle('flip')
 
     if (!hasFlippedCard) {
@@ -43,14 +44,14 @@ function flipCard(){
         checkForMatch();  
 
 }
-
+// compares the data-img attrbute of the two cards
 function checkForMatch () {
     let isMatch = firstCard.dataset.img === secondCard.dataset.img;
 // !!turnery function. refactored an if,else into one line!!
     isMatch ? disableCards() : unflipCards();
     plusMoveCount();
 }
-
+// removes the ability for player to click on matched cards 
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
@@ -58,7 +59,7 @@ function disableCards() {
     resetBoard();
     checkGameEnd();
 }
-
+// removes the abiblity for player to click on flipped cards 
 function unflipCards () {
     lockBoard = true;
     setTimeout(() => {
@@ -69,21 +70,21 @@ function unflipCards () {
         noMatch();
         }, 1000);
 }
-
+// resets the state vars for next move 
 function resetBoard () {
     hasFlippedCard = false;
     lockBoard = false;
     firstCard = null;
     secondCard = null;
 }
-
+// randomizes the order of the cards
 function shuffle () {
     cards.forEach(card => {
         let rndPos = Math.floor(Math.random() * 16);
         card.style.order = rndPos;
     });
 }
-
+// starts timer at 60, if time runs out then game ends 
 function startTimer() {
     timerInterval = setInterval(() => {
       timeLeft--;
@@ -96,7 +97,7 @@ function startTimer() {
       }
     }, 1000);
   }
-
+// checking if all cards have been matched and ends the game
   function checkGameEnd() {
     if (document.querySelectorAll('.game-card.flip').length === cards.length) {
       clearInterval(timerInterval);
@@ -107,7 +108,7 @@ function startTimer() {
     },1000);
     }
   }
-
+// resets game to the initial state 
   function restartGame () {
     clearInterval(timerInterval);
     timeLeft = 60;
@@ -115,7 +116,6 @@ function startTimer() {
     lockBoard = false;
     gameStatus.textContent = 'Begin!';
     
-
     cards.forEach(card => {
         card.classList.remove('flip');
         card.addEventListener('click', flipCard);
